e3fe07546675bd999a086c9b5fb59087f613286d
SRIJAN-KUMAR7 <srijankumar77777@gmail.com>
2024-12-22 22:14:39 +0530

    kk

--- p4.cpp
+++ p4.cpp
@@ -1,80 +1,34 @@
-// #include <bits/stdc++.h>
-// using namespace std;
-
-// int main() {
-// int t;
-// cin >> t;
-// while(t--) {
-// int n,x,y;
-// cin>>n>>x>>y;
-// vector<int> vec(n);
-// for (int i = 0; i < n; i++) {
-// cin >> vec[i];
-// }
-// long long sum = accumulate(vec.begin(), vec.end(), 0LL);
-// long long L= sum-y;
-// long long R= sum-x;
-
-// sort(vec.begin(), vec.end());
-
-// int cnt = 0;
-// for (int i = 0;i<n - 1;i++) {
-// long long low = L - vec[i];
-// long long high = R - vec[i]; 
-
-// int lside = lower_bound(vec.begin() + i + 1, vec.end(), low) - vec.begin();
-// int rside = upper_bound(vec.begin() + i + 1, vec.end(), high) - vec.begin();
-// cnt += (rside - lside);
-// }
-
-// cout << cnt << endl;
-// }
-// return 0;
-// }
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 int main() {
     int t;
-    cin >> t;
-
+    cin >> t;  
     while (t--) {
-        int n, x, y;
-        cin >> n >> x >> y;
-
-        vector<int> a(n);
+        int n;
+        long long x, y;
+        cin >> n >> x >> y;  
+        vector<long long> v(n);
+        long long S = 0;
         for (int i = 0; i < n; i++) {
-            cin >> a[i];
+            cin >> v[i];
+            S += v[i];
         }
-
-        // Calculate total sum S
-        long long S = accumulate(a.begin(), a.end(), 0LL);
-        // Calculate the bounds L and R
-        long long L = S - y;
+long long L = S - y;
         long long R = S - x;
-
-        // Sort the array for binary search
-        sort(a.begin(), a.end());
-
-        int count = 0;
-
-        // Use binary search to count valid pairs for each a[i]
-        for (int i = 0; i < n - 1; i++) {
-            // We are looking for a[j] such that:
-            // L - a[i] <= a[j] <= R - a[i]
-            long long low = L - a[i];
-            long long high = R - a[i];
-
-            // Binary search for the range [low, high]
-            int left = lower_bound(a.begin() + i + 1, a.end(), low) - a.begin();
-            int right = upper_bound(a.begin() + i + 1, a.end(), high) - a.begin();
-
-            // The number of valid pairs (i, j) with j > i
-            count += (right - left);
-        }
-
-        cout << count << endl;
-    }
-
-    return 0;
+        vector<long long> sorted_a = v;
+        sort(sorted_a.begin(), sorted_a.end());
+        long long cnt = 0;
+for (int i = 0; i < n - 1; i++) {
+    long long left_bound = L - sorted_a[i];
+     long long right_bound = R - sorted_a[i];
+    int t = lower_bound(sorted_a.begin() + i + 1, sorted_a.end(), left_bound) - sorted_a.begin();
+    int J = upper_bound(sorted_a.begin() + i + 1, sorted_a.end(), right_bound) - sorted_a.begin();
+    cnt +=(J-t);
+    }   
+cout << cnt << endl;
+ } 
+return 0;
 }
