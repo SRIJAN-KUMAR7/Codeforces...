ce22787de3a75243f5d49fb17b4e229661e3e03c
SRIJAN-KUMAR7 <srijankumar77777@gmail.com>
2024-12-26 13:36:05 +0530

    lk

--- paintthestrip.cpp
+++ paintthestrip.cpp
@@ -6,6 +6,13 @@ ios::sync_with_stdio(false);
 cin.tie(nullptr);
 int t;
 cin >> t;
-while (t--)
+while (t--){
+    int n;
+    cin>>n;
+    vector<int>arr(n);
+    int op;
+    
+
+}
   return 0;
 }
