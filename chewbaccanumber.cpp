9c1aeec25c7fcdd258b18561528b6f144f158092
SRIJAN-KUMAR7 <srijankumar77777@gmail.com>
2024-12-27 23:04:17 +0530

    ff

--- chewbaccanumber.cpp
+++ chewbaccanumber.cpp
@@ -1,23 +1,17 @@
-#include <iostream>
-#include <string>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
     string x;
     cin >> x;
-
-    // Process the first digit
     if (x[0] > '4' && x[0] != '9') {
         x[0] = '9' - x[0] + '0';
     }
-
-    // Process the remaining digits
     for (size_t i = 1; i < x.size(); ++i) {
         if (x[i] > '4') {
             x[i] = '9' - x[i] + '0';
         }
     }
-
-    cout << x << endl;
+    cout <<x<< endl;
     return 0;
 }
