634221c02472f1aae2ffacc421481db16246be92
SRIJAN-KUMAR7 <srijankumar77777@gmail.com>
2024-12-28 20:29:36 +0530

    p1 done

--- lessorequal.cpp
+++ lessorequal.cpp
@@ -6,6 +6,34 @@ ios::sync_with_stdio(false);
 cin.tie(nullptr);
 int n,k;
 cin>>n>>k;
+vector<int>a(n);
+for(int i;i<n;i++){
+    cin>>a[i];
+}
+sort(a.begin(),a.end());
+int mini=a[0];
+int maxi=a[n-1];
+
+if (k==0) {
+        if (a[0]>1) {
+            cout << a[0]-1<< endl;
+        } else {
+            cout<<-1<<endl;
+        }
+    } else{
+        int x = a[k-1];
+        int count = 0;
+        for (int i=0;i<n;i++) {
+            if (a[i]<=x) {
+                count++;
+            }
+        }
+        if (count==k) {
+            cout<<x<<endl;
+        } else {
+            cout<<-1<<endl;
+        }
+    }
 
   return 0;
 }
