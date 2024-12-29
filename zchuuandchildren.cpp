180301764c60c252c25605b24acce5de7aa46595
SRIJAN-KUMAR7 <srijankumar77777@gmail.com>
2024-12-29 22:38:23 +0530

    Z

---
+++ zchuuandchildren.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, m;
+    cin>>n>>m;
+    queue<int>q;  
+    vector<int> candi(n); 
+
+   
+    for (int i = 0; i < n; i++) {
+        cin>>candi[i];
+        q.push(i + 1);  //1-based kr dete hai 
+    }
+
+    int bach_gya= 0;
+    while (!q.empty()) {
+        int current = q.front();  
+        q.pop();
+        candi[current - 1] -= m; 
+        if (candi[current - 1] > 0) {
+            q.push(current); 
+        }
+        bach_gya= current;  
+    }
+    cout <<bach_gya<< endl;  
+    return 0;
+}
