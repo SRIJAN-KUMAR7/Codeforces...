2d8e1f7a2b8e97c5ac9a7ff804eb71d6343d63c5
SRIJAN-KUMAR7 <srijankumar77777@gmail.com>
2025-01-05 19:15:18 +0530

    ddd# Please enter the commit message for your changes. Lines starting

--- digits.cpp
+++ digits.cpp
@@ -60,3 +60,16 @@
 //     solve();
 //     return 0;
 // }
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+ios::sync_with_stdio(false);
+cin.tie(nullptr);
+int t;
+cin >> t;
+while (t--){
+    
+}
+  return 0;
+}
