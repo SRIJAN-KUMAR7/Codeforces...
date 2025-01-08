a6c27d94492dc4da30c18f6a7f82395708909914
SRIJAN-KUMAR7 <srijankumar77777@gmail.com>
2025-01-08 22:50:29 +0530

    p2 done

--- CSES/missingno.cpp
+++ CSES/missingno.cpp
@@ -4,13 +4,15 @@ int main()
 {
 ios::sync_with_stdio(false);
 cin.tie(nullptr);
-int t;
-bool flag =true;
 long long n;
 cin>>n;
-vector<long long>arr;
-for(int i=0;i<n;i++){
+vector<long long>arr(n-1);
+long long s=0;
+for(int i=0;i<n-1;i++){
   cin>>arr[i];
+  s=s+arr[i];
 }
+long long ts=n*(n+1)/2;
+cout<<ts-s<<endl;
   return 0;
 }
