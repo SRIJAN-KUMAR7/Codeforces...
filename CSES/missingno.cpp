9c01d2aa1cc3525122473d2becb4648bd1d3e33b
SRIJAN-KUMAR7 <srijankumar77777@gmail.com>
2025-01-08 22:42:38 +0530

    p2

---
+++ CSES/missingno.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+ios::sync_with_stdio(false);
+cin.tie(nullptr);
+int t;
+bool flag =true;
+long long n;
+cin>>n;
+vector<long long>arr;
+for(int i=0;i<n;i++){
+  cin>>arr[i];
+}
+  return 0;
+}
