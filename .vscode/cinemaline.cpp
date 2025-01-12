d41dffbbec11227e08409ebd0d8c4495ac061395
SRIJAN-KUMAR7 <srijankumar77777@gmail.com>
2025-01-12 15:10:32 +0530

    p1 done

--- .vscode/cinemaline.cpp
+++ .vscode/cinemaline.cpp
@@ -1,25 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main()
-{
-ios::sync_with_stdio(false);
-cin.tie(nullptr);
-int n;
-cin>>n;
-vector<int>a(n);
-for(int i=0;i<n;i++){
-    cin>>a[i];
-}
-int cost=25;
-int c25,c50,c100;
 
-if(a[0]>25){
-    cout<<"NO"<<endl;
-}
-else{
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
 
-}
+    int c25 = 0, c50 = 0;
 
-  return 0;
+    for (int i = 0; i < n; i++) {
+        if (a[i] == 25) {
+            c25++;
+        } else if (a[i] == 50) {
+            if (c25 > 0) {
+                c25--;
+                c50++;
+            } else {
+                cout << "NO\n";
+                return 0;
+            }
+        } else if (a[i] == 100) {
+            if (c50 > 0 && c25 > 0) {
+                c50--;
+                c25--;
+            } else if (c25 >= 3) {
+                c25 -= 3;
+            } else {
+                cout << "NO\n";
+                return 0;
+            }
+        }
+    }
+
+    cout << "YES\n";
+    return 0;
 }
