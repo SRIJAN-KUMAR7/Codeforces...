7752fecf6c18025bbae45fb03e4a5833a702e17f
SRIJAN-KUMAR7 <srijankumar77777@gmail.com>
2025-01-12 15:03:42 +0530

    p1 attempt

---
+++ .vscode/cinemaline.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+ios::sync_with_stdio(false);
+cin.tie(nullptr);
+int n;
+cin>>n;
+vector<int>a(n);
+for(int i=0;i<n;i++){
+    cin>>a[i];
+}
+int cost=25;
+int c25,c50,c100;
+
+if(a[0]>25){
+    cout<<"NO"<<endl;
+}
+else{
+
+
+}
+
+  return 0;
+}
--- .vscode/middleclass.cpp
+++ .vscode/middleclass.cpp
@@ -7,7 +7,19 @@ cin.tie(nullptr);
 int t;
 cin >> t;
 while (t--){
-    
+    int n,x;
+    cin>>n>>x;
+    vector<int>a(n);
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    sort(a.rbegin(),a.rend());
+    long long total=0;
+    for(int i=0;i<n;i++){
+         total+= a[i];
+
+    }
+       
 }
   return 0;
 }
