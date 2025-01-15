d5ff9ced7f3e3bc64214db33903adc1c4d32b975
SRIJAN-KUMAR7 <srijankumar77777@gmail.com>
2025-01-15 09:55:45 +0530

    p1 done

--- .vscode/oddselections.cpp
+++ .vscode/oddselections.cpp
@@ -1,67 +1,126 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+// ios::sync_with_stdio(false);
+// cin.tie(nullptr);
+// int t;
+// cin >> t;
+// while (t--){
+//     int n,x;
+//     cin>>n>>x;
+//     vector<int> a(n);
+//     for(int i=0;i<n;i++){
+//         cin>>a[i];
+//     }
+//     //converting array tom zero and 1
+// for(int i=0;i<n;i++){
+//      if(a[i]&1==1){
+//       a[i]=1;
+//          }
+//         else{
+//       a[i]=0;
+//          }
+//      }
+       
+//        //counting part
+//         int c1=0;
+//         int c0=0;
+//         for(int i=0;i<n;i++){
+//             if(a[i]==1){
+//                 c1++;
+//             }
+//             else{
+//                 c0++;
+//             }
+//         }
+
+// //sufficient 1
+// if(c1>=x){
+//     if(x%2!=0){
+//         cout<<"Yes"<<endl;
+//     }
+//     else{
+//         if(c0>=1){
+//              cout<<"Yes"<<endl;
+//         }
+//     }
+    
+// }
+// //insufficient 1
+// else if(c1<x){
+//     if(c1%2!=0){
+//         cout<<"Yes"<<endl;
+//     }
+//     else{
+//         if(c1>=1){
+//              cout<<"Yes"<<endl;
+//         }
+
+//     }
+// }
+
+    
+// }
+//   return 0;
+// }
+
 #include <bits/stdc++.h>
 using namespace std;
-int main()
-{
-ios::sync_with_stdio(false);
-cin.tie(nullptr);
-int t;
-cin >> t;
-while (t--){
-    int n,x;
-    cin>>n>>x;
-    vector<int> a(n);
-    for(int i=0;i<n;i++){
-        cin>>a[i];
-    }
-    //converting array tom zero and 1
-for(int i=0;i<n;i++){
-     if(a[i]&1==1){
-      a[i]=1;
-         }
-        else{
-      a[i]=0;
-         }
-     }
-       
-       //counting part
-        int c1=0;
-        int c0=0;
-        for(int i=0;i<n;i++){
-            if(a[i]==1){
-                c1++;
-            }
-            else{
-                c0++;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n, x;
+        cin >> n >> x;
+        
+        vector<int> a(n);
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        
+  
+        for (int i = 0; i < n; i++) {
+            a[i] = a[i] % 2; 
+        }
+
+        int c1 = 0, c0 = 0;
+        for (int i = 0; i < n; i++) {
+            if (a[i] == 1) {
+                c1++;  
+            } else {
+                c0++;  
             }
         }
 
-//sufficient 1
-if(c1>=x){
-    if(x%2!=0){
-        cout<<"Yes"<<endl;
-    }
-    else{
-        if(c0>=1){
-             cout<<"Yes"<<endl;
+        
+        if (c1 >= x) {
+            if (x % 2 != 0) {
+                cout << "Yes" << endl;
+            } else {
+                if (c0 >= 1) {
+                    cout << "Yes" << endl;
+                } else {
+                    cout << "No" << endl;
+                }
+            }
+        } else if (c1 < x) {
+            if (c1 % 2 != 0) {
+                cout << "Yes" << endl;
+            } else {
+                if (c1 >= 1 && x != n) {
+                    cout << "Yes" << endl;
+                } else {
+                    cout << "No" << endl;
+                }
+            }
         }
     }
-    
-}
-//insufficient 1
-else if(c1<x){
-    if(c1%2!=0){
-        cout<<"Yes"<<endl;
-    }
-    else{
-        if(c1>=1){
-             cout<<"Yes"<<endl;
-        }
-    }
 
-
-}
-
-    
-}
-  return 0;
+    return 0;
 }
