f2eb97de7e64e528de2aca7fede9c61ddec2037d
SRIJAN-KUMAR7 <srijankumar77777@gmail.com>
2025-01-19 23:49:24 +0530

    p1 done

--- findkdistintictfixedcenter.cpp
+++ findkdistintictfixedcenter.cpp
@@ -9,20 +9,38 @@ cin >> t;
 while (t--){
     int xc,yc,k;
     cin>>xc>>yc>>k;
-    int x=k*xc;
-    int y=k*yc;
-    for(int i=0;i<k;i++){
-        if(i%2==0){
+    int x=xc;
+    int y=yc;
+    for(int i=1;i<=k;i++){
+     if(k%2!=0){
+           if(i!=k){
+        if(i%2==0 ){
             x=x-i;
             y=y-i;
         cout<<x<<" "<<y<<endl;
         }
-        else{
+        else if(i%2!=0){
+        x=x+i;
+        y=y+i;
+        cout<<x<<" "<<y<<endl;
+        }}
+        else if(i==k){
+        cout<<xc<<" "<<yc<<endl;
+        }
+     }
+     else{
+        if(i%2==0 ){
+            x=x-i;
+            y=y-i;
+        cout<<x<<" "<<y<<endl;
+        }
+        else if(i%2!=0){
         x=x+i;
         y=y+i;
         cout<<x<<" "<<y<<endl;
         }
-       
+     }
+        
     }
 }
   return 0;
