8a2d651aa247db68c9b46bf3835bf0536687d16d
SRIJAN-KUMAR7 <srijankumar77777@gmail.com>
2025-01-19 22:32:27 +0530

    p1

--- findkdistintictfixedcenter.cpp
+++ findkdistintictfixedcenter.cpp
@@ -7,7 +7,23 @@ cin.tie(nullptr);
 int t;
 cin >> t;
 while (t--){
-    
+    int xc,yc,k;
+    cin>>xc>>yc>>k;
+    int x=k*xc;
+    int y=k*yc;
+    for(int i=0;i<k;i++){
+        if(i%2==0){
+            x=x-i;
+            y=y-i;
+        cout<<x<<" "<<y<<endl;
+        }
+        else{
+        x=x+i;
+        y=y+i;
+        cout<<x<<" "<<y<<endl;
+        }
+       
+    }
 }
   return 0;
 }
