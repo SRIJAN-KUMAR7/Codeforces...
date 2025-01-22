a2205e9994c92369d6a0af3878344d2b677344a0
SRIJAN-KUMAR7 <srijankumar77777@gmail.com>
2025-01-23 00:30:20 +0530

    p1 done

--- subsequenceupdate.cpp
+++ subsequenceupdate.cpp
@@ -7,9 +7,25 @@ cin.tie(nullptr);
 int t;
 cin >> t;
 while (t--){
-    int n,l,r;
+    long long  n,l,r;
     cin>>n>>l>>r;
-    vector<int>a(n);
+    l--;
+    r--;
+    vector<long long>x(n);
+    for(int i=0;i<n;i++) cin>>x[i];
+    vector<long long>a,b;
+for(int i=l;i<n;i++){
+  a.push_back(x[i]);
+}
+for(int j=0;j<=r;j++){
+  b.push_back(x[j]);
+}
+sort(a.begin(),a.end());
+sort(b.begin(),b.end());
+  long long s1=accumulate(a.begin(),a.begin()+(int)(r-l+1),0LL);
+  long long s2=accumulate(b.begin(),b.begin()+(int)(r-l+1),0LL);
+
+  cout<<min(s1,s2)<<endl;
     
     
 }
