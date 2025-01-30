f83ee3010a84de9c03f7f13085351736d1b66c69
SRIJAN-KUMAR7 <srijankumar77777@gmail.com>
2025-01-30 19:19:45 +0530

    fgfg

--- vladandsumofdigits.cpp
+++ vladandsumofdigits.cpp
@@ -1,3 +1,45 @@
+// // // #include <bits/stdc++.h>
+// // // using namespace std;
+// // // int main()
+// // // {
+// // // ios::sync_with_stdio(false);
+// // // cin.tie(nullptr);
+// // // int t;
+// // // cin >> t;
+// // // while (t--){
+// // // long long  n;
+// // // cin>>n;
+// // // long long r=n%9;
+// // // cout<<45*(n/9)+(r*(r+1))/2<<endl;
+// // // }
+// // //   return 0;
+// // // }
+
+// // #include <bits/stdc++.h>
+// // using namespace std;
+// // long long sd(long long n) {
+// //   long long s = 0;
+// //   for (long long i=1;i<=n;i*=10) {
+// //    s+=(n/(i*10))*45*i+((n%(i*10))*(n%(i*10)+1))/2;
+// //   }
+// //    return s;
+// // }
+
+// // int main() {
+// //     ios::sync_with_stdio(false);
+// //     cin.tie(nullptr);
+
+// //     int t;
+// //     cin >> t;
+// //     while (t--) {
+// //         long long n;
+// //         cin>>n;
+// //         cout<<sd(n)<<"\n";
+// //     }
+// //     return 0;
+// // }
+// // //still wrong
+
 // #include <bits/stdc++.h>
 // using namespace std;
 // int main()
@@ -7,10 +49,18 @@
 // int t;
 // cin >> t;
 // while (t--){
-// long long  n;
-// cin>>n;
-// long long r=n%9;
-// cout<<45*(n/9)+(r*(r+1))/2<<endl;
+//       long long  n;
+//         cin>>n;
+//         long long  sum = 0;
+//         for (int i = 1; i <= n;i++) {
+//           int num = i ;
+//             while (num > 0) {
+//                 sum += num % 10;  
+//                 num /= 10;    
+//             }
+//         }
+//         cout <<sum<< endl;
+    
 // }
 //   return 0;
 // }
@@ -18,25 +68,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-long long sd(long long n) {
-    long long s = 0;
-    for (long long i=1;i<=n;i*=10) {
-        s+=(n/(i*10))*45*i+((n%(i*10))*(n%(i*10)+1))/2;
-    }
-    return s;
-}
+            while (num > 0) {
+                sum += num % 10;  
+                num /= 10;    
+            }
 
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+int main()
+{
+ios::sync_with_stdio(false);
+cin.tie(nullptr);
+int t;
+cin >> t;
+while (t--){
+  int n;
+  cin>>n;
 
-    int t;
-    cin >> t;
-    while (t--) {
-        long long n;
-        cin>>n;
-        cout<<sd(n)<<"\n";
-    }
-    return 0;
 }
-//still wrong
+  return 0;
+}
