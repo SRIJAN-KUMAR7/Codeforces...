daf7805cd2549b6495c23c118db2c41f3e314d13
SRIJAN-KUMAR7 <srijankumar77777@gmail.com>
2025-01-31 20:50:21 +0530

    d

--- CSES/increasingarray.cpp
+++ CSES/increasingarray.cpp
@@ -6,9 +6,17 @@ ios::sync_with_stdio(false);
 cin.tie(nullptr);
 int n;
 cin>>n;
+long long cnt=0;
 vector<int>arr(n);
 for(int i=0;i<n;i++){
-    
+  cin>>arr[i];
 }
+for(int i=1;i<n;i++){
+    if(arr[i-1]>arr[i]){
+      cnt+=arr[i-1]-arr[i];
+      arr[i]=arr[i-1];
+    }
+}
+cout<<cnt<<endl;
   return 0;
 }
