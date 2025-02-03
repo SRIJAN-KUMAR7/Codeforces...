32117a7cfe84d9244f28de37155a1621ab39bb86
SRIJAN-KUMAR7 <srijankumar77777@gmail.com>
2025-02-03 23:12:04 +0530

    tle

--- .vscode/subtractoperation.cpp
+++ .vscode/subtractoperation.cpp
@@ -1,18 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main()
-{
-ios::sync_with_stdio(false);
-cin.tie(nullptr);
-int t;
-cin >> t;
-while (t--){
-    int n,k;
-    cin>>n>>k;
-    vector<int>arr(n);
-    for(int i=0;i<nl;i++){
-        cin>>arr[i];
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, k;
+        cin >> n >> k;
+        vector<int> arr(n);
+        
+        for (int &x : arr) cin >> x;
+
+        bool p = false; 
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) { 
+                if (arr[i] - arr[j] == k || arr[j] - arr[i] == k) {
+                    cout << "YES\n";
+                    p = true;
+                    break;
+                }
+            }
+            if(p) break; 
+        }
+        if(!p) cout << "NO\n";
     }
-}
-  return 0;
+    
+    return 0;
 }
