a1360e7521cf84df0baf062d5e50785ca1b568ed
SRIJAN-KUMAR7 <srijankumar77777@gmail.com>
2025-02-03 23:38:02 +0530

    p1 done

--- .vscode/subtractoperation.cpp
+++ .vscode/subtractoperation.cpp
@@ -1,32 +1,63 @@
+// // #include <bits/stdc++.h>
+// // using namespace std;
+
+// // int main() {
+// //     ios::sync_with_stdio(false);
+// //     cin.tie(0);
+    
+// //     int t;
+// //     cin >> t;
+// //     while (t--) {
+// //         int n, k;
+// //         cin >> n >> k;
+// //         vector<int> arr(n);
+        
+// //         for (int &x : arr) cin >> x;
+
+// //         bool p = false; 
+
+// //         for (int i = 0; i < n; i++) {
+// //             for (int j = i + 1; j < n; j++) { 
+// //                 if (arr[i] - arr[j] == k || arr[j] - arr[i] == k) {
+// //                     cout << "YES\n";
+// //                     p = true;
+// //                     break;
+// //                 }
+// //             }
+// //             if(p) break; 
+// //         }
+// //         if(!p) cout << "NO\n";
+// //     }
+    
+// //     return 0;
+// // }
+
+
 // #include <bits/stdc++.h>
 // using namespace std;
 
 // int main() {
 //     ios::sync_with_stdio(false);
 //     cin.tie(0);
-    
+  
 //     int t;
 //     cin >> t;
 //     while (t--) {
 //         int n, k;
 //         cin >> n >> k;
 //         vector<int> arr(n);
-        
+//         unordered_set<int> s; 
 //         for (int &x : arr) cin >> x;
-
 //         bool p = false; 
-
-//         for (int i = 0; i < n; i++) {
-//             for (int j = i + 1; j < n; j++) { 
-//                 if (arr[i] - arr[j] == k || arr[j] - arr[i] == k) {
-//                     cout << "YES\n";
-//                     p = true;
-//                     break;
-//                 }
+//         for (int x : arr) {
+//             if (s.count(x-k)||s.count(x+k)) { 
+//                 cout << "YES\n";
+//                 p = true;
+//                 break;
 //             }
-//             if(p) break; 
+//             s.insert(x); 
 //         }
-//         if(!p) cout << "NO\n";
+//         if (!p)cout << "NO\n";
 //     }
     
 //     return 0;
@@ -35,30 +66,30 @@
 
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-  
-    int t;
-    cin >> t;
-    while (t--) {
-        int n, k;
-        cin >> n >> k;
-        vector<int> arr(n);
-        unordered_set<int> s; 
-        for (int &x : arr) cin >> x;
-        bool p = false; 
-        for (int x : arr) {
-            if (s.count(x-k)||s.count(x+k)) { 
-                cout << "YES\n";
-                p = true;
-                break;
-            }
-            s.insert(x); 
-        }
-        if (!p)cout << "NO\n";
+int main()
+{
+ios::sync_with_stdio(false);
+cin.tie(nullptr);
+int t;
+cin >> t;
+while (t--){
+    int n,k;
+    cin>>n>>k;
+    bool p=false;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
     }
-    
-    return 0;
+    sort(arr,arr+n);
+    for(int j=0;j<n;j++){
+        int x=arr[j]+k;
+        if(binary_search(arr,arr+n,x)){
+            cout<<"YES"<<endl;
+            p=true;
+            break;
+        }
+    }
+    if(!p){cout<<"NO"<<endl;}
 }
-
+  return 0;
+}
