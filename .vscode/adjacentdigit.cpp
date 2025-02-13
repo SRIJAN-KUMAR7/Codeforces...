a78ca975bb25ff7fdef6a6c50c117d56df1c71a1
SRIJAN-KUMAR7 <srijankumar77777@gmail.com>
2025-02-14 01:31:40 +0530

    jhel

--- .vscode/adjacentdigit.cpp
+++ .vscode/adjacentdigit.cpp
@@ -6,6 +6,18 @@ ios::sync_with_stdio(false);
 cin.tie(nullptr);
 int t;
 cin >> t;
-while (t--)
+while (t--){
+    int x,y;
+    cin>>x>>y;
+    if((x+1)==y){
+        cout<<"Yes"<<endl;
+    }
+    else if(((x-y+1)%9==0)&&x>y){
+        cout<<"Yes"<<endl;
+    }
+    else{
+        cout<<"No"<<endl;
+    }
+}
   return 0;
 }
