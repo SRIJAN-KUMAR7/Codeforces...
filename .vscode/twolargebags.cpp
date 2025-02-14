8aa12a77043046a2c47e77329660b8de00d4d6a0
SRIJAN-KUMAR7 <srijankumar77777@gmail.com>
2025-02-14 13:23:27 +0530

    jj

--- .vscode/twolargebags.cpp
+++ .vscode/twolargebags.cpp
@@ -1,43 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main()
-{
-ios::sync_with_stdio(false);
-cin.tie(nullptr);
-int t;
-cin >> t;
-while (t--){
-    int n;
-    cin>>n;
-    vector<int>arr(n);
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
-    }
-sort(arr.begin(),arr.end());
-vector<int> f(n + 1, 0);
-    for (int i = 0; i < n; i++) {
-    f[arr[i]]++;}
-    int oc=0;
-    bool pos=true;
-    for(int i=0;i<=n;i++){
-        if(f[i]%2==1){
-            oc++;
-            if(i==n || f[i+1]==0){
+ 
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int> f(n+1);
+        for(int i=0;i<n;i++){
+            int a;
+            cin>>a;
+            f[a]++;            
+        }
+        bool pos=true;
+        for(int i=1;i<=n;i++){
+            if(f[i]==1){
                 pos=false;
                 break;
             }
-        f[i]--; 
-        f[i+1]++;
+            f[i+1]+=max(0,f[i]-2);
         }
-
+        cout<<(pos?"YES\n":"NO\n");
     }
-   if(pos){
-    cout<<"YES"<<endl;
-   }
-   else{
-    cout<<"NO"<<endl;
-   }
-
-}
-  return 0;
 }
