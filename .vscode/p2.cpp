56e27fd8dfca60a68177eec55893f3471c59ebf1
SRIJAN-KUMAR7 <srijankumar77777@gmail.com>
2025-03-22 22:04:48 +0530

    jj

--- .vscode/p2.cpp
+++ .vscode/p2.cpp
@@ -2,26 +2,50 @@
 using namespace std;
 int main()
 {
-ios::sync_with_stdio(false);
-cin.tie(nullptr);
-int t;
-cin >> t;
-while (t--){
-    int n;//n>4
-    vector<int>a(n);
-    int cnt0=0;
-    for(int o=0;i<n;i++){
-      cin>>a[i];
-      if(a[i]==0){
-        cnt0++;
-      }
-    }
-    if(cnt0%2==0){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--) {
+        int n; // n > 4
+        cin >> n;
+        vector<int> a(n);
+        int cnt0 = 0;
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+            if (a[i] == 0) {
+                cnt0++;
+            }
+        }
+        int op = 0;
+        vector<pair<int, int>> ops;
+        int l = -1;
+        int r = -1;
 
+        if (cnt0 % 2 == 0) {
+            for (int i = 0; i < n - 1; i++) {
+                if (a[i] == 0 && a[i + 1] == 0) {
+                    l = i + 1;
+                    r = i + 2;
+                    op++;
+                    ops.push_back({l, r});
+                }
+            }
+        } else {
+            for (int i = 0; i < n - 1; i++) {
+                if (a[i] == 0 && a[i + 1] != 0) {
+                    l = i + 1;
+                    r = i + 1;
+                    op++;
+                    ops.push_back({l, r});
+                }
+            }
+        }
+
+        cout << op << "\n";
+        for (auto &p : ops) {
+            cout << p.first << " " << p.second << "\n";
+        }
     }
-    else{
-      
-    }
-}
-  return 0;
+    return 0;
 }
